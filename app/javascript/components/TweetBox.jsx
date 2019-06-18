1cebd661bfe6170e5d4d52b8bd0a2eb8fb398174
samuel santhosh <samuels410@gmail.com>
2019-06-18 23:39:49 +0530

    first commit

---
+++ app/javascript/components/TweetBox.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+ class TweetBox extends React.Component {
+    render () {
+        return (
+            <div>
+                <form >
+                    <textarea />
+                    <label> What's happening</label>
+                    <button>Tweet</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default TweetBox
